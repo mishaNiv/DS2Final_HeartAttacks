9dcaf60d1111b315ee066aae8f80bd0d9445aabc
mnivota <misha.nivota@outlook.com>
2022-11-16 00:54:40 -0800

    improved prediction CSS :D

--- app.R
+++ app.R
@@ -60,20 +60,32 @@ my_ui <- fluidPage(
       mainPanel(
         # colors <https://www.google.com/search?q=%23FF6666&rlz=1C1GCEU_enUS862US862&oq=%23FF6666&aqs=chrome..69i57.297j0j1&sourceid=chrome&ie=UTF-8>
         # information
-        h2("Please enter at least 5 values before trusting your results.", style="color:#d4571e"),
-        h4("Additionally, please understand that there are several more variables that go into this analysis, which are excluded because, unless you've already been admitted for heart disease, you are unlikely to know them off the top of your head, and we're not doctors. Happy heart disease!", style="color:sandybrown"),
+        h2(""),
+        h4("Please enter at least 5 values before trusting your results.", style="color:#d4571e"),
+        h4("Additionally, please understand that there are several more variables 
+           that go into this analysis, which are excluded because, unless you've 
+           already been admitted for heart disease, you are unlikely to know them 
+           off the top of your head, and we're not doctors. Happy heart disease!", 
+           style="color: sandybrown"),
+        textOutput(outputId = "predDefault"),
         # the actual prediction
-        textOutput(outputId = "pred")
+        textOutput(outputId = "pred"),
+        #learned CSS integration from https://stackoverflow.com/questions/24049159/change-the-color-and-font-of-text-in-shiny-app
+        tags$head(tags$style("#predDefault{font-size: 20px;padding-top: 20px;}")),
+        tags$head(tags$style("#pred{font-size: 18px;padding-top: 10px; 
+                             font-style: italic;}"))
         )
       )
     )
   )
 )
 
+
 my_server <- function(input, output) {
 
   output$correlations <- renderPlot ({
     # correlation plot: using custom colors to match heart scheme
+    # from hazel :D
     corr = corrplot(cor(heart), method = "shade",
                     title = "Correlation of Heart Data",
                     col = colorRampPalette(c("white","lightsalmon","brown2"))(100),
@@ -117,6 +129,7 @@ my_server <- function(input, output) {
       var1 = data$slp
     }
     
+    #bar colors from https://www.geeksforgeeks.org/change-color-of-bars-in-barchart-using-ggplot2-in-r/
     baryay = ggplot(data, aes(x=var1, y=output)) + 
       geom_bar(aes(fill = var1), stat='identity') +
       scale_fill_gradient2(mid = "sandybrown",
@@ -129,9 +142,18 @@ my_server <- function(input, output) {
     baryay
   })
   
-  output$pred <- renderText({
+  output$predDefault <- renderText({
+    #creating text for static introduction to prediction
+    str1 = "Using a Random Forest model (which has a 93% accuracy), we have 
+        determined that:"
     
-    #random forest model
+    str1
+  })
+  
+  output$pred <- renderText({
+    #creating dynamic text for prediction
+    
+    #random forest model - from hazel
       hrtmodel <- randomForest(as.factor(output) ~ ., data=data, 
                         importance=TRUE, proximity=TRUE, mtry=1, ntrees=500)
       
@@ -172,14 +194,11 @@ my_server <- function(input, output) {
     
       # responses/outputs based off the prediction
     if (preds == 2) {
-      str = "Using a Random Forest model (which has a 93% accuracy), we have 
-        determined that our random patient is unlikely to have heart problems."
+      str = "A random patient is unlikely to have heart problems."
     } else if (preds == 1) {
-      str = "Using a Random Forest model (which has a 93% accuracy), we have 
-        determined that you are likely to have heart problems. CLOSEST HOSPITAL: Kaiser Permanente Bellevue Medical Center (3.8 stars - 335 reviews!)"
+      str = "You are likely to have heart problems. CLOSEST HOSPITAL: Kaiser Permanente Bellevue Medical Center (3.8 stars - 335 reviews!)"
     } else if (preds == 0) {
-      str = "Using a Random Forest model (which has a 93% accuracy), we have 
-        determined that you are unlikely to have heart problems."
+      str = "You are unlikely to have heart problems."
     }
     
     str
@@ -189,3 +208,6 @@ my_server <- function(input, output) {
 }
 
 shinyApp(ui = my_ui, server = my_server)
+
+
+
